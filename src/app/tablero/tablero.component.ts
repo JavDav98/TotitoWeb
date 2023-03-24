3766d36c398280e058fc5c964a12936b73938173
Javier Dávila <jav98dav@gmail.com>
2023-03-24 01:15:40 -0600

    Problemas con el autoaprendizaje

--- src/app/tablero/tablero.component.ts
+++ src/app/tablero/tablero.component.ts
@@ -11,11 +11,14 @@ export class TableroComponent implements OnInit{
 
   @Output() gameOverEvent = new EventEmitter<PartidaModel>();
   jugadorActual: String = "humano";
-  movimientoH: any[][] = [];
-  movimientoPC: any[][] = []
+  movimientoH: any[] = [];
+  movimientoPC: any[] = [];
   winer: String = "";
   partida: any = {};
   historialPartidas = new Array<any>;
+  player1: String = "humano"
+  jugada: any = [];
+  movjugada: number = 0;
 
   // 0->no jugado, 1->equis-PC, 2->circulo-Humano
   tablero: number[][] = [
@@ -25,7 +28,8 @@ export class TableroComponent implements OnInit{
   ]
 
   ngOnInit() {
-    this.jugadorActual = "humano";
+    this.movjugada = 0;
+    this.player1 = this.jugadorActual;
     this.movimientoPC = [];
     this.movimientoH = [];
     this.winer = "";
@@ -35,6 +39,7 @@ export class TableroComponent implements OnInit{
       [0, 0, 0],
       [0, 0, 0]
     ]
+    this.moverPC(this.jugadorActual)
   }
 
   refresh(historico: Array<any>){
@@ -42,11 +47,14 @@ export class TableroComponent implements OnInit{
     this.historialPartidas = historico;
   }
   mover(fila: number, columna: number, player: String){
-    if (this.tablero[0].includes(0)||this.tablero[1].includes(0)||this.tablero[2].includes(0)) {
-      if (this.winer == "" && this.tablero[fila][columna] === 0) {
+      if (this.winer === "" && this.tablero[fila][columna] === 0) {
         if (this.jugadorActual===player){
           this.tablero[fila][columna] = 2;
-          this.movimientoH.push([fila,  columna]);
+          let mov: any = {
+            fila: fila,
+            columna: columna
+          }
+          this.movimientoH.push(mov);
           setTimeout(() => {
             this.validarGanador(this.jugadorActual)
             this.jugadorActual = "PC";
@@ -54,138 +62,204 @@ export class TableroComponent implements OnInit{
           }, 100);
         }
       }
-    }else if(this.winer == ""){
-      this.prepDataPartida(this.movimientoH, this.movimientoPC, "empate")
-      alert("Es un empate")
-    }
-
   }
 
-  moverPC(player: String){
-    if (this.tablero[0].includes(0)||this.tablero[1].includes(0)||this.tablero[2].includes(0)) {
-      //let fila = 0;
-      //let columna = 0;
-      if(this.historialPartidas.length>0){
-        this.validarSimilitudesPartida();
-      }//else {
-        let fila = Math.floor(Math.random() * 3);
-        let columna = Math.floor(Math.random() * 3);
+  moverPC(player: String){//else {
       //}
-      if (this.winer===""&&this.jugadorActual===player){
-        if (this.tablero[fila][columna] === 0){
-          this.tablero[fila][columna] = 1;
-          this.movimientoPC.push([fila,  columna]);
-          setTimeout(() => {
-            this.validarGanador(this.jugadorActual)
-          }, 100);
-        }else{
-          this.moverPC(this.jugadorActual)
+      if (this.winer===""&&this.jugadorActual==="PC") {
+        if (this.historialPartidas.length > 0) {
+          this.validarSimilitudesPartida();
+        } else {
+          let fila = Math.floor(Math.random() * 3);
+          let columna = Math.floor(Math.random() * 3);
+          if (this.tablero[fila][columna] === 0) {
+            this.tablero[fila][columna] = 1;
+            let mov: any = {
+              fila: fila,
+              columna: columna
+            }
+            this.movimientoPC.push(mov);
+            setTimeout(() => {
+              this.validarGanador(this.jugadorActual)
+            }, 100);
+          } else {
+            this.moverPC(this.jugadorActual)
+          }
+          this.jugadorActual = "humano"
         }
-        this.jugadorActual="humano"
       }
-    }else if(this.winer == ""){
-      this.prepDataPartida(this.movimientoH, this.movimientoPC, "empate")
-      alert("Es un empate")
-    }
+
   }
 
   validarGanador(p: String){
-    if (this.winer===""){
-      if (this.tablero[0][0]===this.tablero[1][1]&&this.tablero[1][1]===this.tablero[2][2]&&this.tablero[2][2]!==0){
-        if (this.tablero[0][0]===1){
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC")
+    if (this.winer === "") {
+      if (this.tablero[0][0] === this.tablero[1][1] && this.tablero[1][1] === this.tablero[2][2] && this.tablero[2][2] !== 0) {
+        if (this.tablero[0][0] === 1) {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC", this.movimientoPC)
           this.winer = "PC";
           alert("¡Perdiste!")
-        }else{
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, p)
+        } else {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, p, this.movimientoH)
           this.winer = p;
           alert("¡Ganaste!")
         }
-      }else if (this.tablero[2][0]===this.tablero[1][1]&&this.tablero[1][1]===this.tablero[0][2]&&this.tablero[0][2]!==0){
-        if (this.tablero[2][0]===1){
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC")
+      } else if (this.tablero[2][0] === this.tablero[1][1] && this.tablero[1][1] === this.tablero[0][2] && this.tablero[0][2] !== 0) {
+        if (this.tablero[2][0] === 1) {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC", this.movimientoPC)
           this.winer = "PC";
           alert("¡Perdiste!")
-        }else{
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, p)
+        } else {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, p, this.movimientoH)
           this.winer = p;
           alert("¡Ganaste!")
         }
-      }else if (this.tablero[2][0]===this.tablero[1][1]&&this.tablero[1][1]===this.tablero[0][2]&&this.tablero[0][2]!==0){
-        if (this.tablero[2][0]===1){
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC")
+      } else if (this.tablero[2][0] === this.tablero[1][1] && this.tablero[1][1] === this.tablero[0][2] && this.tablero[0][2] !== 0) {
+        if (this.tablero[2][0] === 1) {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC", this.movimientoPC)
           this.winer = "PC";
           alert("¡Perdiste!")
-        }else{
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, p)
+        } else {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, p, this.movimientoH)
           this.winer = p;
           alert("¡Ganaste!")
         }
-      }else if (this.tablero[0][0]===this.tablero[0][1]&&this.tablero[0][1]===this.tablero[0][2]&&this.tablero[0][2]!==0){
-        if (this.tablero[0][0]===1){
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC")
+      } else if (this.tablero[0][0] === this.tablero[0][1] && this.tablero[0][1] === this.tablero[0][2] && this.tablero[0][2] !== 0) {
+        if (this.tablero[0][0] === 1) {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC", this.movimientoPC)
           this.winer = "PC";
           alert("¡Perdiste!")
-        }else{
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, p)
+        } else {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, p, this.movimientoH)
           this.winer = p;
           alert("¡Ganaste!")
         }
-      }else if (this.tablero[1][0]===this.tablero[1][1]&&this.tablero[1][1]===this.tablero[1][2]&&this.tablero[1][2]!==0){
-        if (this.tablero[1][0]===1){
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC")
+      } else if (this.tablero[1][0] === this.tablero[1][1] && this.tablero[1][1] === this.tablero[1][2] && this.tablero[1][2] !== 0) {
+        if (this.tablero[1][0] === 1) {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC", this.movimientoPC)
           this.winer = "PC";
           alert("¡Perdiste!")
-        }else{
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, p)
+        } else {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, p, this.movimientoH)
           this.winer = p;
           alert("¡Ganaste!")
         }
-      }else if (this.tablero[2][0]===this.tablero[2][1]&&this.tablero[2][1]===this.tablero[2][2]&&this.tablero[2][2]!==0){
-        if (this.tablero[2][0]===1){
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC")
+      } else if (this.tablero[2][0] === this.tablero[2][1] && this.tablero[2][1] === this.tablero[2][2] && this.tablero[2][2] !== 0) {
+        if (this.tablero[2][0] === 1) {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC", this.movimientoPC)
           this.winer = "PC";
           alert("¡Perdiste!")
-        }else{
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, p)
+        } else {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, p, this.movimientoH)
           this.winer = p;
           alert("¡Ganaste!")
         }
-      }else if (this.tablero[0][0]===this.tablero[1][0]&&this.tablero[1][0]===this.tablero[2][0]&&this.tablero[2][0]!==0){
-        if (this.tablero[0][0]===1){
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC")
+      } else if (this.tablero[0][0] === this.tablero[1][0] && this.tablero[1][0] === this.tablero[2][0] && this.tablero[2][0] !== 0) {
+        if (this.tablero[0][0] === 1) {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC", this.movimientoPC)
           this.winer = "PC";
           alert("¡Perdiste!")
-        }else{
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, p)
+        } else {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, p, this.movimientoH)
           this.winer = p;
           alert("¡Ganaste!")
         }
-      }else if (this.tablero[0][1]===this.tablero[1][1]&&this.tablero[1][1]===this.tablero[2][1]&&this.tablero[2][1]!==0){
-        if (this.tablero[0][1]===1){
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC")
+      } else if (this.tablero[0][1] === this.tablero[1][1] && this.tablero[1][1] === this.tablero[2][1] && this.tablero[2][1] !== 0) {
+        if (this.tablero[0][1] === 1) {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC", this.movimientoPC)
           this.winer = "PC";
           alert("¡Perdiste!")
-        }else{
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, p)
+        } else {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, p, this.movimientoH)
           this.winer = p;
           alert("¡Ganaste!")
         }
-      }else if (this.tablero[0][2]===this.tablero[1][2]&&this.tablero[1][2]===this.tablero[2][2]&&this.tablero[2][2]!==0){
-        if (this.tablero[0][2]===1){
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC")
+      } else if (this.tablero[0][2] === this.tablero[1][2] && this.tablero[1][2] === this.tablero[2][2] && this.tablero[2][2] !== 0) {
+        if (this.tablero[0][2] === 1) {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, "PC", this.movimientoPC)
           this.winer = "PC";
           alert("¡Perdiste!")
-        }else{
-          this.prepDataPartida(this.movimientoH, this.movimientoPC, p)
+        } else {
+          this.prepDataPartida(this.movimientoH, this.movimientoPC, p, this.movimientoH)
           this.winer = p;
           alert("¡Ganaste!")
         }
       }
     }
+    if (!this.tablero[0].includes(0)&&!this.tablero[1].includes(0)&&!this.tablero[2].includes(0)&&this.winer == ""){
+      let a: any;
+      this.winer = "empate"
+      this.prepDataPartida(this.movimientoH, this.movimientoPC, "empate",a)
+      alert("¡Es un empate!")
+    }
+
+
   }
 
   validarSimilitudesPartida(){
+
+    if (this.player1 == "PC"){
+      let partProbables = this.historialPartidas.filter(partida => partida.player1 === "PC");
+      let partidasPCwiner = partProbables.filter(partida => partida.winerPlayer = "PC")
+      let jugadasW = [];
+      for (let p of partidasPCwiner){
+        jugadasW.push(p.movWin)
+      }
+      let uniquePart = jugadasW.filter((value, index, self) => {
+        return self.indexOf(value) === index;
+      });
+      console.log("Jugadas unicas")
+      console.log(uniquePart)
+      if (partidasPCwiner.length > 0){
+        if (this.jugada.length == 0){
+          this.jugada = partidasPCwiner[Math.floor(Math.random() * partidasPCwiner.length)].movWin;
+        }
+        console.log("Jugadas seleccionadas")
+        console.log(this.jugada)
+        let fila = this.jugada[this.movjugada].fila
+        let columna = this.jugada[this.movjugada].columna
+        this.movjugada++
+        if (this.tablero[fila][columna] === 0) {
+          this.tablero[fila][columna] = 1
+          this.jugadorActual = "humano"
+        }
+      }else{
+        let fila = Math.floor(Math.random() * 3);
+        let columna = Math.floor(Math.random() * 3);
+        if (this.tablero[fila][columna] === 0) {
+          this.tablero[fila][columna] = 1;
+          let mov: any = {
+            fila: fila,
+            columna: columna
+          }
+          this.movimientoPC.push(mov);
+          setTimeout(() => {
+            this.validarGanador(this.jugadorActual)
+          }, 100);
+        } else {
+          this.moverPC(this.jugadorActual)
+        }
+        this.jugadorActual = "humano"
+      }
+    }else{
+      let fila = Math.floor(Math.random() * 3);
+      let columna = Math.floor(Math.random() * 3);
+      if (this.tablero[fila][columna] === 0) {
+        this.tablero[fila][columna] = 1;
+        let mov: any = {
+          fila: fila,
+          columna: columna
+        }
+        this.movimientoPC.push(mov);
+        setTimeout(() => {
+          this.validarGanador(this.jugadorActual)
+        }, 100);
+      } else {
+        this.moverPC(this.jugadorActual)
+      }
+      this.jugadorActual = "humano"
+
+    }
+/*
     let mayorPuntaje = 0;
     let partidaspunteadas: any[] = [];
 
@@ -194,23 +268,34 @@ export class TableroComponent implements OnInit{
       for (let f = 0; f < 3; f++){
         for (let c = 0; c < 3; c++){
           if (this.tablero[f][c] == p.tablero[f][c] && this.tablero[f][c]!=0){
-            punteado.punto++;
+            //punteado.punto++;
+          }
+        }
+
+        for (let m of this.movimientoPC){
+          for (let mw of p.movWin){
+            if (m[0] == mw[0] && m[1] == mw[1]){
+              punteado.punto++
+              console.log("Mas 1 punto")
+              console.log(`Movimeintos del PC ${m[0]}`)
+              console.log(`Movimeintos del PC ${m[1]}`)
+              console.log(`Movimeintos del Winer ${mw[0]}`)
+              console.log(`Movimeintos del Winer ${mw[1]}`)
+
+            }
           }
         }
       }
 
       let movWin: any;
-      if  (p.winPlayer == "PC"){
-        movWin = p.movPC
-      }else {
+      if  (p.winPlayer == p.player1 && this.player1 == "PC"){
+        console.log("EL ganador fue p1 y PC es el actual p1")
+        //movWin = p.movPC
+      }else if  (p.winPlayer != p.player1 && this.player1 != "PC"){
+        console.log("EL ganador no fue p1 y pc no es el actual p1")
         movWin = p.movH
       }
 
-      for (let i = 0; i <  this.movimientoPC.length; i++){
-        if  (this.movimientoPC[i] == movWin[i]){
-          punteado.punto++;
-        }
-      }
       if (mayorPuntaje < punteado.punto){
         mayorPuntaje = punteado.punto;
       }
@@ -220,15 +305,17 @@ export class TableroComponent implements OnInit{
 
     let partProbables = partidaspunteadas.filter(partida => partida.punto === mayorPuntaje);
     console.log("Partidas punteadas")
-    console.log(partidaspunteadas)
-    console.log("Terminan *********")
+    console.log(partProbables)
+    console.log("Terminan *********")*/
   }
 
-  prepDataPartida(movH: any, movPC: any, winPlayer: String){
+  prepDataPartida(movH: any, movPC: any, winPlayer: String, movWin: any){
     this.partida.movH = movH;
     this.partida.movPC = movPC;
     this.partida.winPlayer = winPlayer;
+    this.partida.movWin = movWin;
     this.partida.tablero = this.tablero;
+    this.partida.player1 = this.player1
     this.gameOverEvent.emit(this.partida);
   }
 
